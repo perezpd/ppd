856bcffc9fd1f3646de9dbbd1658d4bb00408f10
dieper <saffin2121@gmail.com>
2020-11-04 23:54:49 +0100

    Oracle: first notes about SQL cmd commands

---
+++ oracle/sqlplusnotes.sql
@@ -0,0 +1,24 @@
+-- To connect open Cmd as administrator
+-- >sqlplus
+-- username:sys /as sysdba
+-- pass: ********
+
+-- comprobamos las conexiones dentro de oracle DB
+SELECT name, con_id FROM v$pdbs;
+
+-- the results are 2 databases
+-- We select XEPDB1 with this command
+ALTER SESSION SET CONTAINER = XEPDB1;
+
+-- after changed the selected databas to use we select the opne modes
+
+SELECT name, open_mode FROM v$pdbs;
+
+-- after this command and after set this DB as container The DB is open
+-- we can check anyway with this sentence
+ALTER PLUGGABLE DATABASE open;
+
+-- next time we will use those users:
+-- >SYS
+-- >HR
+-- and another one -to connect to SQL server
