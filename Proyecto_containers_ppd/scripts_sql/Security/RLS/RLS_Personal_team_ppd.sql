5abbb01cddfe66acf1579557442c8e5e8376f8ca
dieper <saffin2121@gmail.com>
2021-05-31 16:10:55 +0200

    Movel audit script to its folder, add new file skeleton for RLS

---
+++ Proyecto_containers_ppd/scripts_sql/Security/RLS/RLS_Personal_team_ppd.sql
@@ -0,0 +1,10 @@
+/* OWNER PEREZ PONTE DIEGO*/
+
+/*********************************************************************************************/
+/**********************        ROW LEVEL SECURITY        *************************************/
+/*************  Hide data ROWS FOR SPECIFIC users depending on the owner *********************/
+/**********  TEST USING USERS: VendedorAngel VendedorVEronica EncargadoLuis  *****************/
+/*********************************************************************************************/
+
+USE [containers_ppd_test];
+GO
